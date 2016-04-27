64d3555eb014b0772c1ddf3f1b833e842e7633a0
David Moeljadi <davidmoeljadi@gmail.com>
2016-04-27 14:23:58 +0800

    add more words

--- chien/chien.py
+++ chien/chien.py
@@ -1,9 +1,18 @@
 cnendict_internal = {
     "我":"I",
-    "喜欢":"like",
+    "你":"you",
+    "他":"he",
+    "她":"she",
+    "小":"small",
+    "大":"big",
     "猫":"cat",
     "狗":"dog",
+    "马":"horse",
     "打":"beat",
+    "喜欢":"like",
     "喂":"feed",
-    "溜":"walk"
+    "溜":"walk",
+    "养":"raise",
+    "爱":"love",
+    "吃":"eat"
 }
