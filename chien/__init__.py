81396a2b55fa0806a2dec4a5d149bccaa9aac1c2
David Moeljadi <davidmoeljadi@gmail.com>
2016-04-27 14:15:24 +0800

    internalize cndict

---
+++ chien/__init__.py
@@ -0,0 +1,2 @@
+from .chien import cnendict_internal as cndict
+__all__ = ["cndict"]
--- chien/chien.py
+++ chien/chien.py
@@ -1,6 +1,9 @@
-cnendict = {
+cnendict_internal = {
     "我":"I",
     "喜欢":"like",
     "猫":"cat",
-    "狗":"dog"
+    "狗":"dog",
+    "打":"beat",
+    "喂":"feed",
+    "溜":"walk"
 }
